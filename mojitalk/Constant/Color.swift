cf15c5c6b60815d40db4abc6f41022286114a960
i-seo725 <hjing719@gmail.com>
2024-01-03 20:02:15 +0900

    Brand 열거형 색상 설정

--- mojitalk/Constant/Color.swift
+++ mojitalk/Constant/Color.swift
@@ -9,7 +9,29 @@ import UIKit
 
 enum Color {
     enum Brand {
+        static var green: UIColor {
+            UIColor(named: "Green")!
+        }
         
+        static var error: UIColor {
+            UIColor(named: "Error")!
+        }
+        
+        static var inactive: UIColor {
+            UIColor(named: "Inactive")!
+        }
+        
+        static var black: UIColor {
+            UIColor(named: "Black")!
+        }
+        
+        static var gray: UIColor {
+            UIColor(named: "Gray")!
+        }
+        
+        static var white: UIColor {
+            UIColor(named: "White")!
+        }
     }
     
     enum Text {
