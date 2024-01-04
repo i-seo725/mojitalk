1d4e0a38f43d6da5aa0c652aad1936e2d897db39
i-seo725 <hjing719@gmail.com>
2024-01-04 22:40:43 +0900

    에셋 이름 수정

---
+++ mojitalk/Scene/OnboardingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  mojitalk
+//
+//  Created by 이은서 on 1/3/24.
+//
+
+import UIKit
+
+class OnboardingViewController: BaseViewController {
+
+//    let onboardingImage = {
+//        let view = UIImage(named: <#T##String#>)
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.Brand.green
+    }
+
+
+}
+
