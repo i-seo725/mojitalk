3dd6156a4bcf91aeb21e064ab23bcb3c286f5c4e
i-seo725 <hjing719@gmail.com>
2024-01-05 00:30:02 +0900

    UI 구성

--- mojitalk/Scene/OnboardingViewController.swift
+++ mojitalk/Scene/OnboardingViewController.swift
@@ -9,12 +9,61 @@ import UIKit
 
 class OnboardingViewController: BaseViewController {
 
+    let contentLabel = {
+        let view = UILabel()
+        view.numberOfLines = 2
+        view.text = "모히톡을 사용하면 어디서나\n팀을 모을 수 있습니다"
+        view.font = .systemFont(ofSize: 22, weight: .bold)
+        view.textColor = .brandBlack
+        view.textAlignment = .center
+        return view
+    }()
+    
     let onboardingImage = {
-        let view = UIImage(named: <#T##String#>)
-    }
+        let view = UIImageView()
+        view.image = Image.onboarding
+        return view
+    }()
+    
+    let startButton = {
+        let view = UIButton()
+        view.setTitle("시작하기", for: .normal)
+        view.backgroundColor = .brandGreen
+        view.setTitleColor(.brandWhite, for: .normal)
+        view.titleLabel?.font = Font.title2
+        view.layer.cornerRadius = 8
+        view.configuration?.contentInsets = .init(top: 11, leading: 16, bottom: 11, trailing: 16)
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configureView() {
+        super.configureView()
+        view.addSubview(contentLabel)
+        view.addSubview(onboardingImage)
+        view.addSubview(startButton)
+    }
+    
+    override func setConstraints() {
+        contentLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(onboardingImage.snp.top).inset(-89)
+            make.horizontalEdges.equalToSuperview().inset(24)
+        }
+        
+        onboardingImage.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().inset(12)
+            make.trailing.equalToSuperview().inset(13)
+        }
+        
+        startButton.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.horizontalEdges.equalToSuperview().inset(24)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(24)
+        }
     }
 
 
