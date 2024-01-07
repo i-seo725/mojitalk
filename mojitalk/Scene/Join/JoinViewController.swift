62e058fe3a644a3add58d5d4a16441ca4370cdd0
i-seo725 <hjing719@gmail.com>
2024-01-07 22:36:33 +0900

    이메일로 회원가입 뷰 파일 생성

---
+++ mojitalk/Scene/Join/JoinViewController.swift
@@ -0,0 +1,17 @@
+//
+//  JoinViewController.swift
+//  mojitalk
+//
+//  Created by 이은서 on 1/7/24.
+//
+
+import UIKit
+import RxSwift
+
+class JoinViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
