5f513f1b472e7cd19791c6dd4c5c3133ff07e0da
i-seo725 <hjing719@gmail.com>
2024-01-08 01:07:13 +0900

    회원가입 페이지로 화면전환 구현

--- mojitalk/Scene/Onboarding/ContinueViewController.swift
+++ mojitalk/Scene/Onboarding/ContinueViewController.swift
@@ -11,9 +11,9 @@ import RxCocoa
 
 class ContinueViewController: BaseViewController {
     
-    let appleLoginButton = ContinueButton(frame: .zero, image: .appleLogin)
-    let kakaoLoginButton = ContinueButton(frame: .zero, image: .kakaoLogin)
-    let emailLoginButton = ContinueButton(frame: .zero, image: .activeWithIcon)
+    let appleLoginButton = ImageButton(frame: .zero, image: .appleLogin)
+    let kakaoLoginButton = ImageButton(frame: .zero, image: .kakaoLogin)
+    let emailLoginButton = ImageButton(frame: .zero, image: .activeWithIcon)
     let joinButton = {
         let fullText = "또는 새롭게 회원가입 하기"
         let attributedString = NSMutableAttributedString(string: fullText)
@@ -30,6 +30,7 @@ class ContinueViewController: BaseViewController {
     }()
     
     var isDisappear: ((Bool) -> ())?
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,6 +75,17 @@ class ContinueViewController: BaseViewController {
             make.height.equalTo(20)
             make.centerX.equalToSuperview()
         }
+    }
+    
+    override func bind() {
+        joinButton.rx.tap
+            .bind(with: self) { owner, _ in
+                let vc = JoinViewController()
+                let nav = UINavigationController(rootViewController: vc)
+                nav.sheetPresentationController?.prefersGrabberVisible = true
+                owner.present(nav, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
 }
