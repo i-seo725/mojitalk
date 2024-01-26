746fe97dea2d0572925a20aaa897446f2dc13051
eunseo <hjing719@gmail.com>
2024-01-26 17:45:58 +0900

    기존 에러 모델 수정

--- mojitalk/Network/Error/Common.swift
+++ mojitalk/Network/Error/Common.swift
@@ -8,14 +8,39 @@
 import Foundation
 
 enum CommonError400: String, Error, LocalizedError {
-    case E01 = "SLP 접근 권한 에러"
-    case E97 = "알 수 없는 라우터 경로"
-    case E05 = "만료된 엑세스 토큰"
-    case E02 = "토큰 인증 실패"
-    case E03 = "계정 정보 조회 실패"
-    case E98 = "과호출 에러"
+    case E01
+    case E97
+    case E05
+    case E02
+    case E03
+    case E98
+    
+    var errorDescription: String? {
+        switch self {
+        case .E01:
+            "SLP 접근 권한 에러"
+        case .E97:
+            "알 수 없는 라우터 경로"
+        case .E05:
+            "만료된 엑세스 토큰"
+        case .E02:
+            "토큰 인증 실패"
+        case .E03:
+            "계정 정보 조회 실패"
+        case .E98:
+            "과호출 에러"
+        }
+    }
+    
 }
 
-enum CommonError500: String, Error {
-    case E99 = "서버 에러"
+enum CommonError500: String, Error, LocalizedError {
+    case E99
+    
+    var errorDescription: String? {
+        switch self {
+        case .E99:
+            "서버 에러"
+        }
+    }
 }
--- mojitalk/Network/Error/UserError.swift
+++ mojitalk/Network/Error/UserError.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-enum UserError: Error, LocalizedError {
+enum UserError: String, Error, LocalizedError {
     case E11
     case E12
-    case E03(isEmail: Bool)
+    case E03
     
     var errorDescription: String {
         switch self {
@@ -18,11 +18,8 @@ enum UserError: Error, LocalizedError {
             "잘못된 요청입니다. 다시 시도해주세요."
         case .E12:
             "중복된 이메일입니다. 다른 이메일을 입력해주세요."
-        case .E03(let isEmail):
-            switch isEmail {
-            case true: "이메일 또는 비밀번호가 일치하지 않습니다."
-            case false: "알 수 없는 계정입니다. 다시 시도해주세요."
-            }
+        case .E03:
+            "계정 정보를 다시 확인해주세요."
         }
     }
 }
