ad4d4a29677e571f476a1da15114fc1ef64c36e1
eunseo <hjing719@gmail.com>
2024-01-26 19:03:34 +0900

    네트워크 매니저 request 메서드 생성

--- mojitalk/Network/NetworkManager.swift
+++ mojitalk/Network/NetworkManager.swift
@@ -15,17 +15,46 @@ class NetworkManager {
     
     let provider = MoyaProvider<UserRouter>()
     
-    func request(endpoint: UserRouter, handler: @escaping ((Response) -> Void)) {
+    func requestEmailValidate(endpoint: UserRouter, handler: @escaping ((Response) -> Void)) {
         provider.request(endpoint) { result in
-            
-            print(result, "\n\n\n")
-            
             switch result {
             case .success(let response):
                 let result = response
                 handler(result)
             case .failure(let error):
                     print(error)
+            }
+        }
+    }
+    
+    func request<T: Decodable>(endpoint: UserRouter, type: T.Type, handler: @escaping ((Result<T, Error>) -> Void)) {
+        provider.request(endpoint) { result in
+            switch result {
+            case .success(let response):
+                let result = try? JSONDecoder().decode(type, from: response.data)
+                if let result {
+                    handler(.success(result))
+                } else {
+                    print("디코딩 에러 발생@@@@@@\n", response)
+                }
+            case .failure(let error):
+                if let errorCode = error.response?.statusCode, let errorData = error.response?.data, let data = try? JSONDecoder().decode(ErrorResponse.self, from: errorData) {
+                    switch errorCode {
+                    case 400:
+                        if let error = CommonError400(rawValue: data.errorCode) {
+                            handler(.failure(error))
+                        } else if let error = UserError(rawValue: data.errorCode) {
+                            handler(.failure(error))
+                        }
+                    case 500:
+                        if let error = CommonError500(rawValue: data.errorCode) {
+                            handler(.failure(error))
+                        }
+                    default:
+                        print("알 수 없는 에러 발생@@@\n", error)
+                        handler(.failure(error))
+                    }
+                }
             }
         }
     }
