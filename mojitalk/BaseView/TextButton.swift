223d470c31645707ea14745e6ed2022e17e0a64b
eunseo <hjing719@gmail.com>
2024-01-27 13:49:08 +0900

    편의생성자 설정

--- mojitalk/BaseView/TextButton.swift
+++ mojitalk/BaseView/TextButton.swift
@@ -27,6 +27,10 @@ class TextButton: UIButton {
         titleLabel?.font = Font.title2
     }
     
+    convenience init(title: String) {
+        self.init(title: title, bgColor: .brandGreen, textColor: .brandWhite)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
