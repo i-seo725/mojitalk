d32e490044d423fcbab37d9cf6dfb302bcf9c8d7
eunseo <hjing719@gmail.com>
2024-02-14 14:29:53 +0900

    UI 디테일 수정

--- mojitalk/Scene/Workspace/WSHomeEmptyViewController.swift
+++ mojitalk/Scene/Workspace/WSHomeEmptyViewController.swift
@@ -12,25 +12,6 @@ import RxCocoa
 class WSHomeEmptyViewController: BaseViewController {
     
     let customNavBar = CustomNavigationBar()
-    let titleSeparatorView = UIView()
-    let leftImage = {
-        let view = UIImageView(image: .vector)
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 8
-        view.backgroundColor = .brandGray
-        view.contentMode = .scaleAspectFit
-        return view
-    }()
-    
-    let titleLabel = {
-        let view = UILabel()
-        view.font = Font.title1
-        view.textAlignment = .left
-        view.numberOfLines = 1
-        view.textColor = .textPrimary
-        view.text = "No Workspace"
-        return view
-    }()
     
     let rightImage = {
         let view = UIImageView(image: .noPhotoCProfile)
@@ -75,11 +56,7 @@ class WSHomeEmptyViewController: BaseViewController {
         super.configureView()
         configureNavBar()
         view.backgroundColor = .backgroundSecondary
-        view.addSubview(leftImage)
-        view.addSubview(titleLabel)
-        view.addSubview(rightImage)
-        view.addSubview(titleSeparatorView)
-        titleSeparatorView.backgroundColor = .seperator
+        view.addSubview(customNavBar)
         view.addSubview(firstLabel)
         view.addSubview(secondLabel)
         view.addSubview(emptyImageView)
@@ -91,32 +68,13 @@ class WSHomeEmptyViewController: BaseViewController {
     }
     
     override func setConstraints() {
-        leftImage.snp.makeConstraints { make in
-            make.size.equalTo(32)
-            make.top.equalToSuperview().offset(52)
-            make.leading.equalToSuperview().offset(16)
+        customNavBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalToSuperview()
+            make.height.equalTo(98)
         }
-                
-        rightImage.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(53)
-            make.size.equalTo(32)
-            make.trailing.equalToSuperview().inset(16)
-        }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(leftImage)
-            make.leading.equalTo(leftImage.snp.trailing).offset(8)
-            make.trailing.equalTo(rightImage.snp.leading).inset(12)
-        }
-
-        titleSeparatorView.snp.makeConstraints { make in
-            make.top.equalTo(leftImage.snp.bottom).offset(16)
-            make.horizontalEdges.equalToSuperview()
-            make.height.equalTo(1)
-        }
-        
+      
         firstLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleSeparatorView.snp.bottom).offset(35)
+            make.top.equalTo(customNavBar.snp.bottom).offset(35)
             make.centerX.equalToSuperview()
         }
         
