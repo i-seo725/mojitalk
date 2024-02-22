e00c2911322822cfcd107dc0b6bb7768267e334d
eunseo <hjing719@gmail.com>
2024-02-22 20:59:30 +0900

    채팅 화면 테이블뷰 틀 잡기

---
+++ mojitalk/Scene/Chat/ChatTableModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatTableModel.swift
+//  mojitalk
+//
+//  Created by 이은서 on 2/22/24.
+//
+
+import Foundation
---
+++ mojitalk/Scene/Chat/ChatTableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  chatTableViewCell.swift
+//  mojitalk
+//
+//  Created by 이은서 on 2/22/24.
+//
+
+import Foundation
