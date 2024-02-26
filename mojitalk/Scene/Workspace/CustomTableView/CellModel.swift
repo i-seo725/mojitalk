14c31261bc8954ad72d8843d2ef2b80fdab6da76
eunseo <hjing719@gmail.com>
2024-02-26 17:18:12 +0900

    셀 모델 설정

--- mojitalk/Scene/Workspace/CustomTableView/CellModel.swift
+++ mojitalk/Scene/Workspace/CustomTableView/CellModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxDataSources
+
+struct CellModel {
+    enum CellImage: String {
+        case hashtagThick
+        case hashtagThin
+        case plusIcon
+    }
+    
+    var text: String
+    var image: CellImage
+    
+    init(text: String, image: CellImage) {
+        self.text = text
+        self.image = image
+    }
+}
+
+struct CustomSection {
+    var header: String
+    var items: [Item]
+}
+
+extension CustomSection: SectionModelType {
+    typealias Item = CellModel
+    
+    init(original: CustomSection, items: [CellModel]) {
+        self = original
+        self.items = items
+    }
+}
